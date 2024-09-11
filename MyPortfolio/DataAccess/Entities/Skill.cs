a342456a9bf803021256636a91491af166d4785d
EmreZiyaEfe <emreziyae@gmail.com>
2024-09-11 22:54:45 +0300

    Created All Entities

---
+++ MyPortfolio/DataAccess/Entities/Skill.cs
@@ -0,0 +1,9 @@
+﻿namespace MyPortfolio.DataAccess.Entities
+{
+    public class Skill
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Value { get; set; }
+    }
+}
