425e818b0fb2d04a81a0dbd5faa1253db248ebed
EmreZiyaEfe <emreziyae@gmail.com>
2024-09-24 01:52:53 +0300

    Added Inbox Page

---
+++ MyPortfolio/Controllers/MessageController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MyPortfolio.DataAccess.Context;
+
+namespace MyPortfolio.Controllers
+{
+	public class MessageController : Controller
+	{
+		MyPortfolioContext context = new MyPortfolioContext();
+		public IActionResult Inbox()
+		{
+			var results = context.Messages.ToList();
+			return View(results);
+		}
+
+		public IActionResult ChangeIsReadToTrue(int id) 
+		{
+			var result = context.Messages.Find(id);
+			result.IsRead = true;
+			context.SaveChanges();
+			return RedirectToAction("Inbox");
+		}
+
+		public IActionResult ChangeIsReadToFalse(int id)
+		{
+			var result = context.Messages.Find(id);
+			result.IsRead = false;
+			context.SaveChanges();
+			return RedirectToAction("Inbox");
+		}
+
+		public IActionResult DeleteMessage(int id)
+		{
+			var result = context.Messages.Find(id);
+			context.Messages.Remove(result);
+			context.SaveChanges();
+			return RedirectToAction("Inbox");
+		}
+
+		public IActionResult MessageDetail(int id)
+		{
+			var result = context.Messages.Find(id);
+			return View(result);
+		}
+	}
+}
