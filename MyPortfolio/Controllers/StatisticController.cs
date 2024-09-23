5970db8a7c7ffdc85d0d44791353ca6d168ed884
EmreZiyaEfe <emreziyae@gmail.com>
2024-09-24 00:41:56 +0300

    Added Statistic Page

---
+++ MyPortfolio/Controllers/StatisticController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MyPortfolio.DataAccess.Context;
+
+namespace MyPortfolio.Controllers
+{
+	public class StatisticController : Controller
+	{
+		MyPortfolioContext context = new MyPortfolioContext();
+		public IActionResult Index()
+		{
+			ViewBag.skill = context.Skills.Count();
+			ViewBag.messagesCount = context.Messages.Count();
+			ViewBag.messagesUnread = context.Messages.Where(m => m.IsRead == false).Count();
+			ViewBag.messagesRead = context.Messages.Where(m => m.IsRead == true).Count();
+			return View();
+		}
+	}
+}
