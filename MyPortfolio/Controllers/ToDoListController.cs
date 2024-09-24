b5db47811ac104778514368d096841c27b5c0321
EmreZiyaEfe <emreziyae@gmail.com>
2024-09-24 19:23:13 +0300

    Added ToDoList

---
+++ MyPortfolio/Controllers/ToDoListController.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MyPortfolio.DataAccess.Context;
+using MyPortfolio.DataAccess.Entities;
+
+namespace MyPortfolio.Controllers
+{
+	public class ToDoListController : Controller
+	{
+		MyPortfolioContext context = new MyPortfolioContext();
+
+		public IActionResult Index()
+		{
+			var results = context.ToDoLists.ToList();
+			return View(results);
+		}
+
+		[HttpGet]
+		public IActionResult CreateToDoList() 
+		{
+			return View();
+		}
+
+		[HttpPost]
+		public IActionResult CreateToDoList(ToDoList toDoList)
+		{
+			toDoList.Status = false;
+			context.ToDoLists.Add(toDoList);
+			context.SaveChanges();
+			return RedirectToAction("Index");
+		}
+
+		public IActionResult DeleteToDoList(int id)
+		{
+			var result = context.ToDoLists.Find(id);
+			context.ToDoLists.Remove(result);
+			context.SaveChanges();
+			return RedirectToAction("Index");
+		}
+
+		[HttpGet]
+		public IActionResult UpdateToDoList(int id)
+		{
+			var result = context.ToDoLists.Find(id);
+			return View(result);
+		}
+
+		[HttpPost]
+		public IActionResult UpdateToDoList(ToDoList toDoList) 
+		{
+			context.ToDoLists.Update(toDoList);
+			context.SaveChanges();
+			return RedirectToAction("Index");
+		}
+
+		public IActionResult ChangeToDoListStatusToTrue(int id)
+		{
+			var result = context.ToDoLists.Find(id);
+			result.Status = true;
+			context.SaveChanges();
+			return RedirectToAction("Index");
+		}
+
+		public IActionResult ChangeToDoListStatusToFalse(int id)
+		{
+			var result = context.ToDoLists.Find(id);
+			result.Status = false;
+			context.SaveChanges();
+			return RedirectToAction("Index");
+		}
+	}
+}
--- MyPortfolio/ViewComponents/LayoutViewComponents/LayoutNavbarComponentPartial.cs
+++ MyPortfolio/ViewComponents/LayoutViewComponents/LayoutNavbarComponentPartial.cs
@@ -1,12 +1,16 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using MyPortfolio.DataAccess.Context;
 
 namespace MyPortfolio.ViewComponents.LayoutViewComponents
 {
 	public class LayoutNavbarComponentPartial : ViewComponent
 	{
+		MyPortfolioContext context = new MyPortfolioContext();
 		public IViewComponentResult Invoke()
 		{
-			return View();
+			ViewBag.toDoListCount = context.ToDoLists.Where(T => T.Status == false).Count();
+			var results = context.ToDoLists.Where(t => t.Status == false).ToList();
+			return View(results);
 		}
 	}
 }
