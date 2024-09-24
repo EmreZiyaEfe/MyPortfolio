d0cb2f6fd7ebc44b47b6af76b7ff8c7a7e4d9f44
EmreZiyaEfe <emreziyae@gmail.com>
2024-09-25 01:19:18 +0300

    Added skills page to admin panel

---
+++ MyPortfolio/Controllers/SkillController.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MyPortfolio.DataAccess.Context;
+using MyPortfolio.DataAccess.Entities;
+
+namespace MyPortfolio.Controllers
+{
+	public class SkillController : Controller
+	{
+		MyPortfolioContext context = new MyPortfolioContext();
+		public IActionResult Index()
+		{
+			var results = context.Skills.ToList();
+			return View(results);
+		}
+
+		[HttpGet]
+		public IActionResult CreateSkill()
+		{
+			return View();
+		}
+
+		[HttpPost]
+		public IActionResult CreateSkill(Skill skill)
+		{
+			context.Skills.Add(skill);
+			context.SaveChanges();
+			return RedirectToAction("Index");
+		}
+
+		public IActionResult DeleteSkill(int id)
+		{
+			var result = context.Skills.Find(id);
+			context.Skills.Remove(result);
+			context.SaveChanges();
+			return RedirectToAction("Index");
+
+		}
+
+		[HttpGet]
+		public IActionResult UpdateSkill(int id)
+		{
+			var result = context.Skills.Find(id);
+			return View(result);
+		}
+
+		[HttpPost]
+		public IActionResult UpdateSkill(Skill skill)
+		{
+			context.Skills.Update(skill);
+			context.SaveChanges();
+			return RedirectToAction("Index");
+		}
+	}
+}
