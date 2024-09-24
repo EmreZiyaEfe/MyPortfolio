314f808e9adcc98ce8500c09af0a6ef768d9cb6a
EmreZiyaEfe <emreziyae@gmail.com>
2024-09-24 23:45:09 +0300

    Added About Section

---
+++ MyPortfolio/Controllers/AboutController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MyPortfolio.DataAccess.Context;
+using MyPortfolio.DataAccess.Entities;
+
+namespace MyPortfolio.Controllers
+{
+	public class AboutController : Controller
+	{
+		MyPortfolioContext context = new MyPortfolioContext();
+		public IActionResult Index()
+		{
+			var result = context.Abouts.ToList();
+			return View(result);
+		}
+
+		[HttpGet]
+		public IActionResult UpdateAbout(int id)
+		{
+			var result = context.Abouts.Find(id);
+			return View(result);
+		}
+
+		[HttpPost]
+		public IActionResult UpdateAbout(About about) 
+		{
+			context.Abouts.Update(about);
+			context.SaveChanges();
+			return RedirectToAction("Index");
+		}
+	}
+}
