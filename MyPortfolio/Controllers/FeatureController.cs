2b020444eb6942c4465dccc98f327e733620608a
EmreZiyaEfe <emreziyae@gmail.com>
2024-09-25 15:48:03 +0300

    Added feature section page to admin panel

---
+++ MyPortfolio/Controllers/FeatureController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MyPortfolio.DataAccess.Context;
+using MyPortfolio.DataAccess.Entities;
+
+namespace MyPortfolio.Controllers
+{
+	public class FeatureController : Controller
+	{
+		MyPortfolioContext context = new MyPortfolioContext();
+		public IActionResult Index()
+		{
+			var result = context.Features.ToList();
+			return View(result);
+		}
+
+		[HttpGet]
+		public IActionResult UpdateFeature(int id)
+		{
+			var result = context.Features.Find(id);
+			return View(result);
+		}
+
+		[HttpPost]
+		public IActionResult UpdateFeature(Feature feature)
+		{
+			context.Features.Update(feature);
+			context.SaveChanges();
+			return RedirectToAction("Index");
+		}
+	}
+}
