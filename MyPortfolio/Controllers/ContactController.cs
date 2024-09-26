09d817e08cdb6f33c8734e5c0ed9a6a9324816f6
EmreZiyaEfe <emreziyae@gmail.com>
2024-09-26 19:29:37 +0300

    Added method for home page contact section

---
+++ MyPortfolio/Controllers/ContactController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MyPortfolio.DataAccess.Context;
+using MyPortfolio.DataAccess.Entities;
+
+namespace MyPortfolio.Controllers
+{
+	public class ContactController : Controller
+	{
+		MyPortfolioContext context = new MyPortfolioContext();
+		public IActionResult Index()
+		{
+			var results = context.Contacts.ToList();
+			return View(results);
+		}
+
+		[HttpGet]
+		public IActionResult UpdateContact(int id)
+		{
+			var result = context.Contacts.Find(id);
+			return View(result);
+		}
+
+		[HttpPost]
+		public IActionResult UpdateContact(Contact contact)
+		{
+			context.Contacts.Update(contact);
+			context.SaveChanges();
+			return RedirectToAction("Index");
+		}
+	}
+}
--- MyPortfolio/ViewComponents/ContactComponentPartial.cs
+++ MyPortfolio/ViewComponents/ContactComponentPartial.cs
@@ -1,11 +1,19 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using MyPortfolio.DataAccess.Context;
 
 namespace MyPortfolio.ViewComponents
 {
     public class ContactComponentPartial : ViewComponent
     {
+        MyPortfolioContext context = new MyPortfolioContext();
         public IViewComponentResult Invoke()
         {
+            ViewBag.contactTitle = context.Contacts.Select(x => x.Title).SingleOrDefault();
+            ViewBag.contactDescription = context.Contacts.Select(x => x.Description).SingleOrDefault();
+            ViewBag.contactEmail = context.Contacts.Select(x => x.Email).SingleOrDefault();
+            ViewBag.contactPhone = context.Contacts.Select(x => x.Phone).SingleOrDefault();
+            ViewBag.contactAddress = context.Contacts.Select(x => x.Address).SingleOrDefault();
+
             return View();
         }
     }
